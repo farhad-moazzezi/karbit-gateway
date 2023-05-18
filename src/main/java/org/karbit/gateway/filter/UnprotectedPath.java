15834f977055b76fde94bc6550d94dff15c9a12e
farhad moazzezi <f.moazzezi@mydigipay.com>
2023-05-18 17:18:27 +0330

    remove activation from UnprotectedPath - for user activation request should be open session before

--- src/main/java/org/karbit/gateway/filter/UnprotectedPath.java
+++ src/main/java/org/karbit/gateway/filter/UnprotectedPath.java
@@ -7,7 +7,6 @@ public enum UnprotectedPath {
 	LOGOUT_URL("/user/auth/logout"),
 	LOGIN_URL("/user/auth/login"),
 	SIGNUP_URL("/user/auth/signup"),
-	ACTIVATE_URL("/user/auth/activate"),
 	GET_ARTICLE_SUMMARY_URL("doc/article/summary/**");
 
 
